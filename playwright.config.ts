3cdd74e025557746b0f5e994442281483c5df83a
admalik10pearls <ahmad.amir@10pearls.com>
2026-01-29 15:22:05 +0500

    refactor(config): refine projects and clean up config file
    add seperate projects for ui browsers and add seperate project for api

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,40 +5,50 @@ dotenv.config();
 export default defineConfig({
   globalSetup: './global-setup.ts',
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://www.saucedemo.com',
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    storageState: 'auth.json',
   },
 
   /* Configure projects for major browsers */
   projects: [
+    // ---------- UI PROJECTS ----------
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'ui-chromium',
+      testIgnore: ['**/api/**'],
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: 'https://www.saucedemo.com',
+        storageState: 'auth.json',
+      },
+    },
+    {
+      name: 'ui-firefox',
+      testIgnore: ['**/api/**'],
+      use: {
+        ...devices['Desktop Firefox'],
+        baseURL: 'https://www.saucedemo.com',
+        storageState: 'auth.json',
+      },
+    },
+    {
+      name: 'ui-webkit',
+      testIgnore: ['**/api/**'],
+      use: {
+        ...devices['Desktop Safari'],
+        baseURL: 'https://www.saucedemo.com',
+        storageState: 'auth.json',
+      },
     },
 
+    // ---------- API PROJECT ----------
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: 'api',
+      testMatch: ['tests/api/*.spec.ts'],
     },
   ],
 });
