f0ded8c588172c5662bb497c737d55f31913ec43
admalik10pearls <ahmad.amir@10pearls.com>
2026-02-13 16:07:00 +0500

    refactor(strings): add error string for locked user

--- shared/constants/strings.ts
+++ shared/constants/strings.ts
@@ -3,6 +3,7 @@
  */
 export const STRINGS = {
   loginErrorText: 'Epic sadface: Username and password do not match any user in this service',
+  loginErrorTextLockedOut: 'Epic sadface: Sorry, this user has been locked out.',
 } as const;
 
 export type String = (typeof STRINGS)[keyof typeof STRINGS];
