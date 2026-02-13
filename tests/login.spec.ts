a090271e7d6dd8a8f8b2407e7b4501f54339c396
admalik10pearls <ahmad.amir@10pearls.com>
2026-02-13 16:08:19 +0500

    test(login): add login tests that use params from testdata json file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,8 +1,15 @@
+import path from 'path';
+
 import { test, expect } from '@playwright/test';
 
 import { ROUTES } from '../shared/constants/routes.ts';
 import { STRINGS } from '../shared/constants/strings.ts';
 import { LoginPage } from '../shared/pages/login.page.ts';
+import { loadTestData } from '../shared/utilities/data-loader.ts';
+
+const __dirname = import.meta.dirname;
+const userDataPath = path.join(__dirname, '../testdata/loginData.json');
+const userData = loadTestData(userDataPath);
 
 test.describe('Login Functionality', () => {
   const user = process.env.APP_USER!;
@@ -27,4 +34,24 @@ test.describe('Login Functionality', () => {
     await expect(page.getByText(STRINGS.loginErrorText)).toBeVisible();
     await expect(page).not.toHaveURL(ROUTES.inventory);
   });
+  test(
+    'user is unable to login with invalid password (credentials driven by test data json file)',
+    { tag: '@smoke' },
+    async ({ page }) => {
+      const login = new LoginPage(page);
+      await login.login(user, userData.invalidUser.password);
+      await expect(page.getByText(STRINGS.loginErrorText)).toBeVisible();
+      await expect(page).not.toHaveURL(ROUTES.inventory);
+    },
+  );
+  test(
+    'user is unable to login with invalid username (credentials driven by test data json file)',
+    { tag: '@smoke' },
+    async ({ page }) => {
+      const login = new LoginPage(page);
+      await login.login(userData.invalidUser.username, pass);
+      await expect(page.getByText(STRINGS.loginErrorTextLockedOut)).toBeVisible();
+      await expect(page).not.toHaveURL(ROUTES.inventory);
+    },
+  );
 });
